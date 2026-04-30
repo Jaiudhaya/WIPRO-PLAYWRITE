72c724f162e23404743123d64b7e72de794ca6f7
Jai Udhaya <jaiudhaya@gmail.com>
2026-04-30 12:29:10 +0530

    Math ops, String Ops, Logical Ops

---
+++ Day1/index.js
@@ -0,0 +1,23 @@
+let a = 12;
+let b = 17;
+
+let str = "Hello!!!";
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+console.log("String:", str);
+console.log("Length of string:", str.length);
+console.log("Uppercase:", str.toUpperCase());
+console.log("Lowercase:", str.toLowerCase());
+console.log("Substring (0-5):", str.substring(0, 5));
+
+console.log("a > b:", a > b);
+console.log("a < b:", a < b); 
+console.log("a == b:", a == b);
+console.log("a != b:", a != b);
+console.log("a >= b:", a >= b);
+console.log("a <= b:", a <= b);
