bc1a648ca2824774962391998001b8c2e3976c3b
Jai Udhaya <jaiudhaya@gmail.com>
2026-05-05 12:41:31 +0530

    Promise, Fetch API

--- Day4/Index.js
+++ Day4/Index.js
@@ -18,3 +18,23 @@ getData().then((data) => {
 // setInterval(() => {
 //     console.log("Checking for new data...");
 // }, 3000);
+
+function getData() {
+    return new Promise((resolve, reject) => {
+        if(2 > 3) {
+ 
+            setTimeout(() => {
+                resolve("Data received");
+            }, 2000);
+        } else {
+            reject('not working')
+        }
+    });
+}
+ 
+fetch("https://jsonplaceholder.typicode.com/todos")
+    .then(res => res.json())
+    .then(data => data.filter(d => d.id % 2 === 0))
+    .then(data => data.map(d => (d.title = "Hello " + d.title)))
+    .then(data => console.log(data))
+    .catch(error => console.error("Error fetching data:", error));
