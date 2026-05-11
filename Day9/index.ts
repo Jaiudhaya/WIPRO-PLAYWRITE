5ac68a9f65811abed5c40c3ba1ca172535f249cc
Jai Udhaya <jaiudhaya@gmail.com>
2026-05-11 12:47:29 +0530

    Basics

--- Day9/index.ts
+++ Day9/index.ts
@@ -1,2 +1,15 @@
 let name = "TypeScript";
 console.log(name);
+
+let name1: string = "Diana";
+let result: number | string;
+result = 45;
+result = "HiHi";
+console.log(name1, result);
+ 
+function add(a: number, b: number): string {
+    return "Sum: " + (a + b);
+}
+ 
+console.log(add(2, 3));
+ 
