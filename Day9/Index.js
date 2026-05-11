c1fd272c5d34d1ce8a615831dd62a6f150ff499d
Jai Udhaya <jaiudhaya@gmail.com>
2026-05-11 12:16:37 +0530

    Trial

--- Day9/Index.js
+++ Day9/Index.js
@@ -1,36 +1,35 @@
-function welcome(name = "Guest") { // default params
+//Default parameters
+function greet(name = "Guest") { // default params
     console.log(`Welcome ${name}!`);
 }
 
-welcome("Aaryan")
-welcome("Arush")
-welcome()
+greet("Jai")
+greet("Udhaya")
+greet()
 
-const nums = [1, 2, 3, 4, 5]; // spread operator
-const newNums = [...nums, 6, 7, 8];
-console.log(newNums);
 
-const obj = {name: "aryan"};
-const updatedObj = {...obj, age: 25};
-console.log(updatedObj);
+//Spread operator
+const num = [1, 2, 3, 4, 5]; 
+const newNum = [...num, 6, 7, 8];
+console.log(newNum);
 
-const cal = { // rest operator
+const obj = {name: "Jai"};
+const updated = {...obj, age: 22};
+console.log(updated);
+
+
+//Rest operators
+const cal = { 
     sum: (...nums) => nums.reduce((a, b) => a + b, 0), // rest parameters
 }
+console.log(cal.sum(1, 2, 3, 4, 5));
 
-console.log(cal.sum(1, 2, 43, 4))
 
-// destructuring
-// const arr = ["Aryan", "Arush", "Vivek", "Rohit"];
-// const first = arr[0];
-// const second = arr[1];
-// const third = arr[2];
-// const fourth = arr[3];
-
-const [first, second, third, fourth, fifth] = ["Aryan", "Arush", "Vivek", "Rohit"]; // pos-based extraction
-const [one, two, ...three] = [1, 3, 5, 6, 7];
-console.log(first, second, third, fourth, fifth);
+//DECONSTRUCTOR
+const [first, second, third] = ["Jai", "Udhaya", "Annie"]; // pos-based extraction
+const [one, two, ...three] = [1, 2, 3, 4, 5];
+console.log(first, second, third);
 console.log(one, two, three)
 
-const { name, age } = {name: "aryan", age: 25}; // key-based extracted
+const { name, age } = {name: "Jai", age: 22}; // key-based extracted
 console.log(name, age);
